05bd6856aaba04b9bc13f20e94b5da8ac6302ccc
Abdulqudus <abdulqudusabubakre@gmail.com>
2021-08-23 15:20:24 +0100

    Modified readme for unique pathss

--- unique-paths/js/solution.js
+++ unique-paths/js/solution.js
@@ -3,18 +3,21 @@
  * @param {number} n
  * @return {number}
  */
- var uniquePaths = function(m, n) {
-  const myArray = Array(m).fill(0).map(() => Array(n).fill(0));
-  
+var uniquePaths = function (m, n) {
+  const myArray = Array(m)
+    .fill(0)
+    .map(() => Array(n).fill(0));
+
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (i == 0 || j == 0) {
-        myArray[i][j] = 1
+        myArray[i][j] = 1;
       } else {
-        myArray[i][j] = parseInt(myArray[i - 1][j]) + parseInt(myArray[i][j - 1])
+        myArray[i][j] =
+          parseInt(myArray[i - 1][j]) + parseInt(myArray[i][j - 1]);
       }
     }
   }
-  
-  return myArray[m-1][n-1]
+
+  return myArray[m - 1][n - 1];
 };
